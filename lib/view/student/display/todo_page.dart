05f481190897ec918e022cfe3acd4955672d97cd
@irshadpukayoor@gmail.com <irshadpukayoor@gmail.com>
2024-01-28 22:31:05 +0530

    updated

--- lib/view/student/display/todo_page.dart
+++ lib/view/student/display/todo_page.dart
@@ -8,7 +8,7 @@ class TodoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Column(
         children: [
           Card(
